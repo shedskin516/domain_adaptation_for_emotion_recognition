de49b58dfa0a3d920fb2ff37b43f75f00c33cd60
Yongkang Du <yongkangdu@Yongkangs-MacBook-Pro.local>
2023-04-24 00:12:08 -0700

    Modify distance

--- contrast/distance.py
+++ contrast/distance.py
@@ -10,25 +10,23 @@ import torch.nn.functional as F
 
 from sklearn.decomposition import PCA
 
-def get_pairs(path_aff, path_sewa):
-    aff_features = np.load(path_aff)
-    sewa_features = np.load(path_sewa)
+def get_pairs(source_feature, target_feature, n_clusters):
+    source_feature = source_feature.detach().numpy()
+    target_feature = target_feature.detach().numpy()
 
-    n_clusters = 10
+    source_kmeans = KMeans(n_clusters=n_clusters).fit(source_feature)
+    source_cluster_centers = source_kmeans.cluster_centers_
+    source_labels = source_kmeans.labels_
 
-    aff_kmeans = KMeans(n_clusters=n_clusters).fit(aff_features)
-    aff_cluster_centers = aff_kmeans.cluster_centers_
-    aff_labels = aff_kmeans.labels_
-
-    sewa_kmeans = KMeans(n_clusters=n_clusters).fit(sewa_features)
-    sewa_cluster_centers = sewa_kmeans.cluster_centers_
-    sewa_labels = sewa_kmeans.labels_
+    target_kmeans = KMeans(n_clusters=n_clusters).fit(target_feature)
+    target_cluster_centers = target_kmeans.cluster_centers_
+    target_labels = target_kmeans.labels_
 
     wasserstein_distances = np.zeros((n_clusters, n_clusters))
 
     for i in range(n_clusters):
         for j in range(n_clusters):
-            wasserstein_distances[i,j] = wasserstein_distance(aff_cluster_centers[i], sewa_cluster_centers[j])
+            wasserstein_distances[i,j] = wasserstein_distance(source_cluster_centers[i], target_cluster_centers[j])
             # print(i,j,wasserstein_distances[i,j])
 
     # print(wasserstein_distances)
@@ -40,27 +38,7 @@ def get_pairs(path_aff, path_sewa):
         print("aff cluster", i, "is paired with sewa cluster", col_ind[i])
 
     # Allocate identical numbers to the related clusters in both datasets."
-    for i in range(len(aff_labels)):
-        aff_labels[i] = col_ind[aff_labels[i]]
+    for i in range(len(source_labels)):
+        source_labels[i] = col_ind[source_labels[i]]
 
-
-    # plot
-    # plt.scatter(aff_cluster_centers[:,0], aff_cluster_centers[:,1], c='red', marker='o', label='aff')
-    # plt.scatter(sewa_cluster_centers[:,0], sewa_cluster_centers[:,1], c='blue', marker='s', label='sewa')
-    # for i in range(n_clusters):
-    #     plt.annotate(str(i), xy=(aff_cluster_centers[i,0], aff_cluster_centers[i,1]), color='red')
-    #     plt.annotate(str(col_ind[i]), xy=(sewa_cluster_centers[col_ind[i],0], sewa_cluster_centers[col_ind[i],1]), color='blue')
-    # plt.legend()
-    # plt.xlabel('Dimension 1')
-    # plt.ylabel('Dimension 2')
-    # plt.savefig("images/figure1.png")
-
-    # plot clusters
-    # reduced_data = PCA(n_components=2).fit_transform(sewa_features)
-    # # print(reduced_data.shape)
-    # kmeans = KMeans(n_clusters=10, random_state=0).fit(reduced_data)
-    # plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=kmeans.labels_, s=50, cmap='viridis')
-    # plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=200, marker='*', c='black')
-    # plt.show()
-
-    return aff_labels, sewa_labels
+    return source_labels, target_labels
